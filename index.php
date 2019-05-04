829114285b81b414d35fc6b4e56830d491cc58c1
Rob Duarte <Robert.duarte@uvm.edu>
2019-05-04 19:58:22 -0400

    Added major pages, some initial css

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+
+    <head>
+        <title>CS 142 Final</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Robert Duarte">
+        <meta name="description" content="A site to hold all my course work for CS.">
+        <link rel='stylesheet' href='final.css'>
+            
+    </head>
+
+    
+    <body id="body">
+        <h1 class="pageTitle">Robert Duarte</h1>
+
+    
+        <nav class="navbar" role="navigation">
+                <div class="navLinks">
+
+                    <!-- mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ol class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="courses.php">Courses</a></li>
+                            <li><a href="profWork.php">Professional Work</a></li> 
+                            <li><a href="addWork.php">Additional Work</a></li>
+                           
+                        </ol>
+                    </div>
+                    <!-- /.navbar-collapse -->
+                </div>
+        </nav>
+        
+        
+        <h2>About Me</h2>
+        <article class="aboutMe">I am a Junior at UVM studying computer science. I am from Amherst, New Hampshire. I grew up loving 
+        hockey, water sports, and winter. I chose to come to UVM because I love New England, the area is beautiful, 
+        and I thought it was a great school for me. Throughout my years here I have completed many projects, most of 
+        which are now sitting on my laptop never to be seen again. I plan to change that. This site is my 
+        final project for CS 142, and it will act as a portfolio for all of my CS work done while at UVM. </article>
+        
+               
+
+    </body>
+</html>
---
+++ profWork.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+
+    <head>
+        <title>CS 142 Final</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Robert Duarte">
+        <meta name="description" content="A site to hold all my course work for CS.">
+        <link rel='stylesheet' href='final.css'>    
+        
+    </head>
+    
+    <body id="body">
+        <h1 class="pageTitle">Professional Work</h1>
+        
+        
+        <nav class="navbar" role="navigation">
+                <div class="navLinks">
+
+                    <!-- mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ol class="nav navbar-nav">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="courses.php">Courses</a></li>
+                            <li class="active"><a href="profWork.php">Professional Work</a></li> 
+                            <li><a href="addWork.php">Additional Work</a></li>
+                           
+                        </ol>
+                    </div>
+                    <!-- /.navbar-collapse -->
+                </div>
+        </nav>
+    </body>
