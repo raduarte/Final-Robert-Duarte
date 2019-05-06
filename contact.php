d468627fa981ee966100816d958bd59bfff87da9
Rob Duarte <Robert.duarte@uvm.edu>
2019-05-06 12:01:48 -0400

    Added contact.php, style tweaks

---
+++ contact.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+
+    <head>
+        <title>CS 142 Final</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Robert Duarte">
+        <meta name="description" content="A site to hold all my course work for CS.">
+        <link rel='stylesheet' href='final.css'>
+            
+    </head>
+
+    
+    <body id="body">
+
+    
+        <nav class="navbar" role="navigation">
+                <div class="navLinks">
+
+                    <!-- mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ol class="nav navbar-nav">
+                            <li id="topBar">Robert Duarte</li>
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="courses.php">Courses</a></li>
+                            <li><a href="profWork.php">Professional Work</a></li> 
+                            <li><a href="addWork.php">Additional Work</a></li>
+                           
+                        </ol>
+                    </div>
+                    <!-- /.navbar-collapse -->
+                </div>
+        </nav>
+        
+        <h1 class="pageTitle">Contact</h1>
+        
+        <hr class="breaks">
+        
+        <div class="contactMe">
+        <p>
+            Please feel free to contact me with any questions, concerns, or suggestions! <br>
+        </p>
+        <p>
+            Email: Robert.Duarte@uvm.edu <br>
+            Phone: (603) 809-7861
+            
+        </p>
+        </div>      
+        
+       
+        
+               
+        <footer class="footer">
+            <hr class="breaks">
+            <p>Page made by Robert Duarte</p>
+            <p>Last update: 5/6/19</p>
+            
+        </footer>
+        <a href="contact.php" class="contact">Contact me!</a>
+        
+        
+    </body>
+    
+    
+</html>
